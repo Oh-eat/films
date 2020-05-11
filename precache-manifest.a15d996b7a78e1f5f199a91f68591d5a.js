55347f5f47a50f3dbd930a66c11090e8d20bd742
Wangwange <ltk6031@naver.com>
2020-05-11 21:48:08 +0900

    Updates

---
+++ precache-manifest.a15d996b7a78e1f5f199a91f68591d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ff60fbb3b0176baeb4269c606d9bb6",
+    "url": "/films/index.html"
+  },
+  {
+    "revision": "83df1637fb1c7ee50b69",
+    "url": "/films/static/css/main.46fdbbe4.chunk.css"
+  },
+  {
+    "revision": "2442060cc089b6ef6639",
+    "url": "/films/static/js/2.4a86a5b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/films/static/js/2.4a86a5b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83df1637fb1c7ee50b69",
+    "url": "/films/static/js/main.36ec9c37.chunk.js"
+  },
+  {
+    "revision": "5eb8b999abaebb13b765",
+    "url": "/films/static/js/runtime-main.82567c96.js"
+  }
+]);
